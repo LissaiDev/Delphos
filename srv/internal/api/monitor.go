b20619ec8845db89eb15297e4596e5d78226838f
René Armindo Lissai <lissai@lissai.lissai>
2025-07-06 02:46:12 +0200

    FEAT(logs): Log incoming requests and outgoing responses

--- srv/internal/api/monitor.go
+++ srv/internal/api/monitor.go
@@ -2,13 +2,16 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/LissaiDev/Delphos/internal/monitor"
+	"github.com/LissaiDev/Delphos/pkg/logger"
 )
 
 func GetMonitorInfo(w http.ResponseWriter, r *http.Request) {
+	logger.Log.Info(fmt.Sprintf("<-- Incoming request: %s", r.URL.String()))
 	stats, err := monitor.GetStats()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -17,16 +20,18 @@ func GetMonitorInfo(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(stats)
+	logger.Log.Info(fmt.Sprintf("--> Response sent: %s", r.URL.String()))
 }
 
 func GetMonitorInfoSSE(w http.ResponseWriter, r *http.Request) {
+	logger.Log.Info(fmt.Sprintf("<-- Incoming request: %s", r.URL.String()))
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("cache-control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
 	flusher, ok := w.(http.Flusher)
 	if !ok {
-		http.Error(w, "Streming is not supported", http.StatusInternalServerError)
+		http.Error(w, "Streaming is not supported", http.StatusInternalServerError)
 		return
 	}
 
@@ -43,5 +48,6 @@ func GetMonitorInfoSSE(w http.ResponseWriter, r *http.Request) {
 
 		flusher.Flush()
 		time.Sleep(time.Second * 5)
+		logger.Log.Info(fmt.Sprintf("--> Response sent: %s", r.URL.String()))
 	}
 }
